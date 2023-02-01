6bbd19679ee4403916a4f57867e9be15535f783a
Anargyros Sigalas <111334536+ans-sigalas@users.noreply.github.com>
2023-02-01 14:04:20 +0900

    Update FundMe.sol

--- lesson-4/tweaked-contract/FundMe.sol
+++ lesson-4/tweaked-contract/FundMe.sol
@@ -1,1 +1,44 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.8;
+
+import "https://github.com/ans-sigalas/full-blockchain-solidity-course-js/blob/main/lesson-4/PriceConverter.sol";
+
+contract FundMe {
+
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    
+    address public owner;
+    
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minimumUsd, "You need to send at least 1 Eth!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    } 
+
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sender is not the owner of this contract!");
+        _;
+    }
+}
