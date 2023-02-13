e3d142486b83ec94b3b21d2b96ed7290208f3cd5
Anargyros Sigalas <zerotoblockchaindev@gmail.com>
2023-02-13 13:28:34 +0900

    Add prettier plugin and .prettierrc mod

--- lesson-5/deploy.js
+++ lesson-5/deploy.js
@@ -50,101 +50,101 @@
 */
 
 // Import dependencies and external packages
-const ethers = require("ethers"); // const similar to let. const makes it so ethers variable cannot be changed. require is a function to import ethers package
-const fs = require("fs-extra"); // To install package we need to run $ yarn add fs-extra
-require("dotenv").config(); // To call the dotenv tool.
+const ethers = require("ethers") // const similar to let. const makes it so ethers variable cannot be changed. require is a function to import ethers package
+const fs = require("fs-extra") // To install package we need to run $ yarn add fs-extra
+require("dotenv").config() // To call the dotenv tool.
 
 async function main() {
-  // http://127.0.0.1:7545 <- Ganache RPC Server
+    // http://127.0.0.1:7545 <- Ganache RPC Server
 
-  // Below is our connection to the blockchain
-  const provider = new ethers.providers.JsonRpcProvider(
-    // "http://127.0.0.1:7545" This is not actually something that we need to secure, however, maybe we would like to use an API key or a certain endpoint that only we want to have access to.
-    process.env.RPC_URL
-  ); // We say that we are going to connect to the url inside the bracket.
+    // Below is our connection to the blockchain
+    const provider = new ethers.providers.JsonRpcProvider(
+        // "http://127.0.0.1:7545" This is not actually something that we need to secure, however, maybe we would like to use an API key or a certain endpoint that only we want to have access to.
+        process.env.RPC_URL
+    ) // We say that we are going to connect to the url inside the bracket.
 
-  // Now that we have an encrypted key we can pass it instead of using const wallet like below.
-  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
-  // Then we are going to create a wallet from this encrypted key.
-  let wallet = new ethers.Wallet.fromEncryptedJsonSync( // The reason we used let is to connect our wallet back to our provider.
-    encryptedJson,
-    process.env.PRIVATE_KEY_PASSWORD
-  );
-  wallet = await wallet.connect(provider); // To connect our wallet to the provider.
+    // Now that we have an encrypted key we can pass it instead of using const wallet like below.
+    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8")
+    // Then we are going to create a wallet from this encrypted key.
+    let wallet = new ethers.Wallet.fromEncryptedJsonSync( // The reason we used let is to connect our wallet back to our provider.
+        encryptedJson,
+        process.env.PRIVATE_KEY_PASSWORD
+    )
+    wallet = await wallet.connect(provider) // To connect our wallet to the provider.
 
-  // Now, to run our deploy.js we need to say $ PRIVATE_KEY_PASSWORD=password node deploy.js
-  // Then we run $ history -c to clear our history because someone can find our password from our terminal history
+    // Now, to run our deploy.js we need to say $ PRIVATE_KEY_PASSWORD=password node deploy.js
+    // Then we run $ history -c to clear our history because someone can find our password from our terminal history
 
-  //Below is our wallet
-  // const wallet = new ethers.Wallet( // Import new wallet from ganache
-  //   // "b26801e16b664597d257e9eb5813b5da3c1521c618850be463cea3de692c3b9f", // private key | Of note: Pasting your private key directly into your code is a no-no. We will learn to avoid it in the future.
-  //   // To be even more secure, we will encrypt our private key using a script inside a file called "encryptKey.js"
-  //   process.env.PRIVATE_KEY,
-  //   provider
-  // );
-  // If we were to push the online, people would be able to see our private key.
-  // One way to avoid that is by using a .env file or an "Environment Variable Fine".
-  // We can set this variables on our terminal but for now we are going to set them in a .env file.
-  // .env is a file where we will store sensitive information and we are never going to share with anybody.
-  // This .env file will choose variables of our choosing into the environment of our code.
-  // Of note regarding the .env file: Some tools might need a "0x" in front of our private key, but ethers and hardhat don't.
-  // To be able to grab our private key from the .env file and put it on our deploy.js file, we have to download a tool called dotenv.
-  // We do that by running $yarn add dotenv
+    //Below is our wallet
+    // const wallet = new ethers.Wallet( // Import new wallet from ganache
+    //   // "b26801e16b664597d257e9eb5813b5da3c1521c618850be463cea3de692c3b9f", // private key | Of note: Pasting your private key directly into your code is a no-no. We will learn to avoid it in the future.
+    //   // To be even more secure, we will encrypt our private key using a script inside a file called "encryptKey.js"
+    //   process.env.PRIVATE_KEY,
+    //   provider
+    // );
+    // If we were to push the online, people would be able to see our private key.
+    // One way to avoid that is by using a .env file or an "Environment Variable Fine".
+    // We can set this variables on our terminal but for now we are going to set them in a .env file.
+    // .env is a file where we will store sensitive information and we are never going to share with anybody.
+    // This .env file will choose variables of our choosing into the environment of our code.
+    // Of note regarding the .env file: Some tools might need a "0x" in front of our private key, but ethers and hardhat don't.
+    // To be able to grab our private key from the .env file and put it on our deploy.js file, we have to download a tool called dotenv.
+    // We do that by running $yarn add dotenv
 
-  // In order to deploy our contract we are going to need the ABI and the BIN codes of the contract.
-  // For that, we need to read from the two files that we created previously.
-  // To do that, we need to use a package called "fs".
-  // Below we call the ABI.
-  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+    // In order to deploy our contract we are going to need the ABI and the BIN codes of the contract.
+    // For that, we need to read from the two files that we created previously.
+    // To do that, we need to use a package called "fs".
+    // Below we call the ABI.
+    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
 
-  // Below we call the BIN.
-  const binary = fs.readFileSync(
-    "./SimpleStorage_sol_SimpleStorage.bin",
-    "utf8"
-  );
+    // Below we call the BIN.
+    const binary = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.bin",
+        "utf8"
+    )
 
-  // Now that we have the BIN and the ABI we can create something called a contract factory.
-  // In ethers a contract factory is just an object that you can use to deploy contracts.
-  // Below we call the contract factory object.
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  // We passed the abi so that our code knows how to interact with contract.
-  // We passed the binary because this is the main compiled code in our wallet so that we have a private key we can use to sign while deploying this contract.
+    // Now that we have the BIN and the ABI we can create something called a contract factory.
+    // In ethers a contract factory is just an object that you can use to deploy contracts.
+    // Below we call the contract factory object.
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+    // We passed the abi so that our code knows how to interact with contract.
+    // We passed the binary because this is the main compiled code in our wallet so that we have a private key we can use to sign while deploying this contract.
 
-  console.log("Deploying, please wait...");
+    console.log("Deploying, please wait...")
 
-  // We can now deploy this contract with ethers with the following:
-  const contract = await contractFactory.deploy(); // This tells our code to stop here and wait for contract to be deployed.
-  // If we want to add some specifics about our contract deployment like gas price, gas limit, etc. we have to use the override function which can be found within our "deploy" definition.
-  // We can find the contractFactory.deploy(); override by pressing CTRL + click on the "deploy" function.
-  // This will take us into the function so we can see anything about this function (e.g., how it's defined, etc. )
-  // We can add overrides in our deploy function by doing the following: const contract = await contractFactory.deploy({gasLimit:100000}); / gasLimit was an example
-  // Another thing we can do is we can wait a certain amount of blocks to make sure that it will actually get attached to the chain.
-  // We can do that by doing the following:
-  await contract.deployTransaction.wait(1);
-  // This way we specified that we want to wait 1 block for confirmation.
-  // For that we have to run the following:
-  // console.log("Here is the deployment transaction: ");
-  // console.log(contract.deployTransaction);
-  // console.log("Here is the transaction receipt: ");
-  // console.log(transactionReceipt);
-  // console.log(contract);
+    // We can now deploy this contract with ethers with the following:
+    const contract = await contractFactory.deploy() // This tells our code to stop here and wait for contract to be deployed.
+    // If we want to add some specifics about our contract deployment like gas price, gas limit, etc. we have to use the override function which can be found within our "deploy" definition.
+    // We can find the contractFactory.deploy(); override by pressing CTRL + click on the "deploy" function.
+    // This will take us into the function so we can see anything about this function (e.g., how it's defined, etc. )
+    // We can add overrides in our deploy function by doing the following: const contract = await contractFactory.deploy({gasLimit:100000}); / gasLimit was an example
+    // Another thing we can do is we can wait a certain amount of blocks to make sure that it will actually get attached to the chain.
+    // We can do that by doing the following:
+    await contract.deployTransaction.wait(1)
+    // This way we specified that we want to wait 1 block for confirmation.
+    // For that we have to run the following:
+    // console.log("Here is the deployment transaction: ");
+    // console.log(contract.deployTransaction);
+    // console.log("Here is the transaction receipt: ");
+    // console.log(transactionReceipt);
+    // console.log(contract);
 
-  const currentFavoriteNumber = await contract.retrieve(); // To retrieve our favorite number.
-  // console.log("Here is the current favorite number: ");
-  // console.log(currentFavoriteNumber.toString());
-  // Instead of the above, we can make it more syntactical correct by using something called string interpolation, to interpolate our string with variables.
-  // Typically for JavaScript strings we use double quotes "" but if we want to include variables with strings we can use backticks `` instead (located next to button 1).
-  // ${} means that it includes a function.
-  console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
-  const transactionResponse = await contract.store("7");
-  const transactionReceipt = await transactionResponse.wait(1);
-  const updatedFavoriteNumber = await contract.retrieve();
-  console.log(`Updated Favorite Number: ${updatedFavoriteNumber}`);
+    const currentFavoriteNumber = await contract.retrieve() // To retrieve our favorite number.
+    // console.log("Here is the current favorite number: ");
+    // console.log(currentFavoriteNumber.toString());
+    // Instead of the above, we can make it more syntactical correct by using something called string interpolation, to interpolate our string with variables.
+    // Typically for JavaScript strings we use double quotes "" but if we want to include variables with strings we can use backticks `` instead (located next to button 1).
+    // ${} means that it includes a function.
+    console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`)
+    const transactionResponse = await contract.store("7")
+    const transactionReceipt = await transactionResponse.wait(1)
+    const updatedFavoriteNumber = await contract.retrieve()
+    console.log(`Updated Favorite Number: ${updatedFavoriteNumber}`)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
