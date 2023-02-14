5c9ce824c9cdc14d9a6d86d13d1cc11cce47dd1c
Anargyros Sigalas <zerotoblockchaindev@gmail.com>
2023-02-14 17:35:32 +0900

    Add const for checking and updating current value

--- lesson-6/scripts/deploy.js
+++ lesson-6/scripts/deploy.js
@@ -1,6 +1,7 @@
 const { ethers, run, network } = require("hardhat")
 
 async function main() {
+    //Contract deployment
     const SimpleStorageFactory = await ethers.getContractFactory(
         "SimpleStorage"
     )
@@ -9,12 +10,24 @@ async function main() {
     await simpleStorage.deployed()
     console.log(`Deployed contract to: ${simpleStorage.address}`)
     console.log(network.config)
+    // If we are deploying on a testnet (Goerli in this instance), this verifies our contract on Etherscan
     if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
         await simpleStorage.deployTransaction.wait(6) // wait 6 blocks
         await verify(simpleStorage.address, [])
     }
+
+    //Check current value
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current value is: ${currentValue}`)
+
+    //Update current value
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value is: ${updatedValue}`)
 }
 
+// Etherscan contract verify function definition
 async function verify(contractAddress, args) {
     console.log("Verifying contract...")
     try {
