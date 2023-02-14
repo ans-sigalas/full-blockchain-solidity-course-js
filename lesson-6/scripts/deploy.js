b381243fbc4b224740c6a695fc131c99b810e773
Anargyros Sigalas <zerotoblockchaindev@gmail.com>
2023-02-14 15:39:31 +0900

    Add automatic code verification for etherscan

--- lesson-6/scripts/deploy.js
+++ lesson-6/scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
@@ -8,6 +8,27 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.deployed()
     console.log(`Deployed contract to: ${simpleStorage.address}`)
+    console.log(network.config)
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6) // wait 6 blocks
+        await verify(simpleStorage.address, [])
+    }
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 main()
